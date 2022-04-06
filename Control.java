b1ca51bbef30080c2bc901257b4d61219924688c
SamuelH-O <51359738+samuelh-o@users.noreply.github.com>
2022-04-06 09:36:03 +0200

    ClientSocket done

--- Control.java
+++ Control.java
@@ -2,26 +2,72 @@ package client_robi;
 
 import static java.util.logging.Level.SEVERE;
 
-import java.io.File;
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
 
 public class Control {
-    private File fic;
+	public TextArea ipTextArea;
+	public TextArea portTextArea;
+	public Label errorLabel;
+	private File fic;
 
-    public Button b1;
-    public TextArea textArea1;
-    public TextArea textArea2;
+    public Button execButton;
+    public TextArea commandTextArea;
+    public TextArea traceTextArea;
     
     public void initialize() {
     }
 
-    public void b1_exec(ActionEvent event) {
-    	System.out.println("b1_exec");
-    	System.out.println("textArea1 = "+textArea1.getText());
+    public void execButton_exec(ActionEvent event) {
+		String ip;
+		Message command;
+		Message trace;
+		int port;
+		if (portTextArea.getText().isEmpty()) {
+			errorLabel.setText("Erreur : pas de port");
+		} else if (ipTextArea.getText().isEmpty()) {
+			errorLabel.setText("Erreur : pas d'IP");
+		} else if (commandTextArea.getText().isEmpty()) {
+			errorLabel.setText("Erreur : pas de commande");
+		}else {
+			errorLabel.setText("");
+			ip = ipTextArea.getText();
+			port = Integer.parseInt(portTextArea.getText());
+			command = new Message("t",commandTextArea.getText());
+			try {
+				Socket clientSocket = new Socket(ip, port);
+				PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				writer.println("Connected ?");
+				if (reader.readLine().equals("Connected !")) {
+					writer.println(Message.toJson(command));
+					trace = Message.fromJson(reader.readLine());
+					traceTextArea.setText(trace.getMess());
+				} else {
+					traceTextArea.setText("Erreur : échec connection");
+					System.out.println("Erreur : échec connection");
+				}
+				writer.close();
+				reader.close();
+				clientSocket.close();
+			} catch (UnknownHostException e) {
+				errorLabel.setText("Erreur : UnknownHostException");
+				e.printStackTrace();
+			} catch (IOException e) {
+				errorLabel.setText("Erreur : IOException");
+				e.printStackTrace();
+			}
+		}
+		System.out.println("execButton_exec");
+    	System.out.println("commandTextArea = "+commandTextArea.getText());
     }
     
     public void openFile(ActionEvent event) {
@@ -47,4 +93,8 @@ public class Control {
     }
     public void saveFile(ActionEvent event) {
     }
+
+	public void exit(ActionEvent actionEvent) {
+		Platform.exit();
+	}
 }
